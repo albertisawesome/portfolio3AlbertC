9a6cd2b9b2f824c165144fbb78c10d46826947e9
Albert Chen <chenalbert60@gmail.com>
2015-01-15 15:01:55 -0800

    TROLL

--- public_html/script.js
+++ public_html/script.js
@@ -5,92 +5,85 @@
  */
 
 
-$("document").ready(function(){
-$(".bob").css("background-color", "gray");    
-$(".sam").css("background-color", "orange");    
-$('p:last').css({
-    "background-color": "yellow"
-});
-$('p:first').css({
-    "background-color": "pink"
-
-        
-});
+$("document").ready(function() {
 
 
+    $('#woot').accordion({header: "h3"});
+    $(".bob").css("background-color", "gray");
+    $(".sam").css("background-color", "orange");
+    $('p:last').css({
+        "background-color": "yellow"
+    });
+    $('p:first').css({
+        "background-color": "pink"
 
-        //$('')     
-        $('#replaceWText').bind('click', replaceWText);   
-        $('#randPara').bind('click', addAPara);
-        $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
-        $('h1').bind('click', mouseClick);
-        $("h2").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
-        $('h2').bind('click', mouseClick);
-        $("document").ready(function(){
+
+    });
+    //$('')     
+    $('#replaceWText').bind('click', replaceWText);
+    $('#randPara').bind('click', addAPara);
+    $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+    $('h1').bind('click', mouseClick);
+    $("h2").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+    $('h2').bind('click', mouseClick);
+    $("document").ready(function() {
         $('#hideLogo').bind('click', hideTheImage);
         $('#showLogo').bind('click', showTheImage);
-        
+    });
+    function fadeTheImage() {
+        $('#logo').fadeOut(2500);
+    }
 });
 
-function fadeTheImage(){
-    $('#logo').fadeOut(2500);
-}
 
-function fadeALittle(){
+function fadeALittle() {
     $('#logo').fadeTo(2500, .30);
-    
-function toggleTheText(){
-    $('h4').toggle(2500);   
+}
+function toggleTheText() {
+    $('h4').toggle(2500);
 }
 
-function showTheImage(){
+function showTheImage() {
     $('#logo').show('fold')
 }
 
-function hideTheImage(){
+function hideTheImage() {
     $('#logo').hide('explode', {}, 2500);
 }
 
-function mouseOutMe(){
-    
+function mouseOutMe() {
+
 }
 
-function removeAPara(){
-$('#randPara p:last').remove();
+function removeAPara() {
+    $('#randPara p:last').remove();
 }
-function addAPara(){
-$('#randPara').append('<p>ADDED</p>');
+function addAPara() {
+    $('#randPara').append('<p>ADDED</p>');
 }
-function replaceWText(){
-$('#replaceWText').text('Replaced!');
+function replaceWText() {
+    $('#replaceWText').text('Replaced!');
 }
-function mouseOverMe(){
+function mouseOverMe() {
     $("h1").html("READ AND LISTEN TO THIS SONG ;D");
 }
 
-function mouseOverMe(){
+function mouseOverMe() {
     $('h1').html('U MAD?');
 }
-function mouseClick(){
+function mouseClick() {
     $('p').html('JUST KIDDING');
-    
 }
 
-function mouseOverMe(){
+function mouseOverMe() {
     $("h2").html("TROLL");
 }
 
-function mouseOverMe(){
+function mouseOverMe() {
     $('h2').html('U NOW HAVE VIRUSES ;)');
 }
 function mouseClick() {
     $('p').html('JUST KIDDING')
-    }    
-    
-<form action="">
-<button type="button" id="hideLogo">Hide the Logo</button>
-<button type="button" id="showLogo">Show the Logo</button>
-<button type="button" id="fadeLogo">Fade the Logo</button>
-<button type="button" id="fadeALittle">Fade a Little</button>
-        
-</form>
+
+}
+
